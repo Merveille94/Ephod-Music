f36b4331428ad72a9f3ce218719b6e8a46e58571
Merveille94 <emanueleguehi@gmail.com>
2025-01-28 13:47:17 +0000

    navbar scroll behavior added

--- app/components/SignInComponents/ArtistPage.tsx
+++ app/components/SignInComponents/ArtistPage.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const ArtistPage = () => {
     return (
-        <div className="min-h-screen bg-gray-900 text-gray-500 text-3xl flex items-center justify-center">
-            <div className="container mx-auto p-6 flex items-center justify-center">
-                Connection to Database not established yet ...
+        <div className="min-h-[80vh] bg-gray-900 text-gray-500 text-3xl flex items-center justify-center">
+            <div className="container mx-auto p-6 flex flex-col items-center justify-center">
+                <div>😓</div>
+                <p className="text-lg text-center">Connection to Database not established yet ...</p>
             </div>
         </div>
     )
