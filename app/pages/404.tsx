991e5413e228da4b6b56ca2b3fa06a91a442ead0
Merveille94 <emanueleguehi@gmail.com>
2025-01-28 12:45:00 +0000

    navbar scroll behavior added

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Custom404() {
     return (
         <div className="h-screen flex justify-center items-center">
-            <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+            <h1 className="text-3xl font-bold pb-4">404 - Page Not Found</h1>
+            <Link href="/">Click me!!</Link>
         </div>
     );
 }
